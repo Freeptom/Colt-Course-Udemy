35b608d6a8246c8d2c492fca02f63d1a1e8359d4
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-04-11 07:16:19 +0100

    moving onto functions

---
+++ Javascript/functionsProblemSet.js
@@ -0,0 +1,23 @@
+
+// console.log("isEven?");
+// function isEven(num){
+//   if(num % 2 === 0) {
+//   return num + " is even!";
+//   }
+//   else {
+//     return num + " is not even";
+//   }
+// };
+
+//refactor
+console.log("isEven?");
+function isEven(num){
+  return num % 2 ===0;
+}
+
+console.log("Factorial");
+//e.g. 6 is 6 x 5 x 4 x 3 x 2 x 1 so, number multiplied by every number
+//below that. Factorial of 0 is 1.
+
+console.log("Kebab to Snake");
+//kebabToSnake() take single kebab-cased sring argument and returns the snake_case version
