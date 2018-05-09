2ba0e882c446fd4f4e0354bf14c1645f3fb9f85e
Tom Pearson <tpearson@HeadlandEnt.local>
2018-05-09 17:23:48 +0100

    finished problem set

--- Javascript/arrayProblemSet.js
+++ Javascript/arrayProblemSet.js
@@ -1,11 +1,5 @@
 //1. write function printReverse() which takes array as an argument and prints out the elements in reverse order
 
-//printReverse([1,2,3,4]);
-//4
-//3
-//2
-//1
-//**HINT - use a loop (forEach?)
 
 console.log('printReverse');
 function printReverse(arr) {
@@ -18,7 +12,6 @@ printReverse([3,6,2,5]);
 
 
 //2. write function isUniform() - returns true if all elements in array are identical
-//**HINT - use loop, and use variable which keeps track of first variable in index. Compare to next item in index. If not equal, escape.
 
 console.log('isUniform');
 function isUniform(arr) {
@@ -34,16 +27,15 @@ function isUniform(arr) {
 
 //3. write function sumArray() - accepts an array of numbers and returns the sum of all numbers in the array
 
-//**Hint - use variable which holds result and constantly adds into it each time it loops
+
 
 console.log('sumArray');
 function sumArray(arr) {
-  //look at first index
-  var result = 0;
-  sumArray.forEach(function(element) {
-    result = result+element;
+  var total = 0;
+  arr.forEach(function(element) {
+   total+=element;
   });
-  return result;
+  return total;
 }
 
 sumArray([1,2,3,4]);
@@ -51,4 +43,28 @@ sumArray([1,2,3,4]);
 
 //4. write function max() - accepts an array of numbers and returns the max number
 
-//**Hint - use variable which holds max number
+// console.log('max');
+// function max(arr) {
+//   var max = 0;
+//   arr.forEach(function(element) {
+//    if (element > max) {
+//      max = element;
+//    }
+//   });
+//   return max;
+// }
+//
+//
+//
+// max([1,2,6,4]);
+
+console.log('max');
+function max(arr) {
+  var max = arr[0];
+  for(var i = 1; i < array.length; i++){
+    if(arr[i] > max){
+    max = arr[i];
+    }
+  }
+  return max;
+}
