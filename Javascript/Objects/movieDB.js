515fa1ee50075a094c0c2d7a6c2b246f204974a1
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-05-10 07:19:42 +0100

    started Objects Movie DB

---
+++ Javascript/Objects/movieDB.js
@@ -0,0 +1,9 @@
+//create an array of movie objects. Each movie should have a title, rating and hasWatched property. Iterate through the array and print out
+
+var movieDatabase {
+  movies: [
+    {name: 'Die Hard', rating: 8.1, hasWatched: false},
+    {name: 'No Country for Old Men', rating: 9.2, hasWatched: true},
+    {name: 'Spy', rating: 7.8, hasWatched: false}
+  ]
+}
---
+++ Javascript/Objects/objectsQuiz.js
@@ -0,0 +1,13 @@
+var someObject = {
+friends: [
+  {name: "Malfoy"},
+  {name: "Crabbe"},
+  {name: "Goyle"}
+],
+color: "baby blue",
+isEvil: true
+};
+
+//access Malfoy:
+// someObject.friends
+//someObject.friends[0]
