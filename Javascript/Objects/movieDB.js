8e960430bac815beafc14b52e39f60f01ec12e44
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-05-12 17:29:40 +0100

    MovieDB finished

--- Javascript/Objects/movieDB.js
+++ Javascript/Objects/movieDB.js
@@ -1,9 +1,36 @@
 //create an array of movie objects. Each movie should have a title, rating and hasWatched property. Iterate through the array and print out
 
-var movieDatabase {
-  movies: [
-    {name: 'Die Hard', rating: 8.1, hasWatched: false},
-    {name: 'No Country for Old Men', rating: 9.2, hasWatched: true},
-    {name: 'Spy', rating: 7.8, hasWatched: false}
+var movies = [
+    {
+      title: 'Die Hard',
+      rating: 8.1,
+      hasWatched: false
+    },
+    {
+      title: 'No Country for Old Men',
+      rating: 9.2,
+      hasWatched: true
+    },
+    {
+      title: 'Spy',
+      rating: 7.8,
+      hasWatched: false
+    }
   ]
+
+movies.forEach(function(movie){
+  buildString(movie)
+  console.log(buildString(movie));
+});
+
+function buildString(movie) {
+  var result = 'You have ';
+  if(movie.hasWatched) {
+    result += 'watched ';
+  } else {
+    result += 'not seen ';
+  }
+  result += '\'' + movie.title + '\' - ';
+  result += movie.rating + ' stars';
+  return result;
 }
