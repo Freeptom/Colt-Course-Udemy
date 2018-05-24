460e708d88d6d06a9e90f50e92769d813a695b7c
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-05-24 21:41:05 +0100

    finished colorGame 3

--- Javascript/Color Game/colorGame.js
+++ Javascript/Color Game/colorGame.js
@@ -7,25 +7,41 @@ var colors = [
 'rgb(255, 0, 255)' //All Red and All Blue
 ]
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
 
-colorDisplay.textContent = pickedColor;
+
+colorDisplay.textContent = pickedColor;// replace span with pickedColor
 
 for(var i = 0; i < squares.length; i++) {
   // add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
 
   // add click listeners to squares
-
   squares[i].addEventListener('click', function() {
     // get color of clicked square
     var clickedColor = this.style.backgroundColor;
     // compare color to pickedColor
     if(clickedColor === pickedColor){
-      alert('correct!')
+      messageDisplay.textContent = 'Correct!';
+      changeColors(clickedColor);
     }else {
-      alert('Wrong!')
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = 'Try again';
     }
   });
+}
+
+function changeColors (color) {
+  // loop through all squares
+  for(i = 0; i < colors.length; i++) {
+      // change color for each to match given color
+      squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
