591e97fe8c11f34177c1cbb623c3959d367a1e94
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-06-06 13:43:19 +0100

    finished intro to Express

--- Backend/IntroToExpress/FirstExpressApp/app.js
+++ Backend/IntroToExpress/FirstExpressApp/app.js
@@ -11,9 +11,25 @@ app.get('/bye', function(req, res){
     res.send('goodbye');
 });
 // '/cat' => 'Meow!'
-app.get('/cat', function(req, res){
+app.get('/dog', function(req, res){
     res.send('MEOW');
 });
+
+app.get('/r/:subredditName', function(req, res){
+    var subreddit = req.params.subredditName;
+    res.send('WELCOME TO THE ' + subreddit.toUpperCase() + ' SUBREDDIT!');
+});
+
+app.get('/r/:subredditName/comments/:id/:title', function(req, res){
+    console.log(req.params);
+    res.send('Welcome to the comments page!');
+});
+
+app.get('*', function(req, res){
+    res.send('You are a star');
+});
+
+
 app.listen(3000, process.env.IP, function(){
     console.log('server has started');
 });
