2248ea849f321624488e20399561f8142bdd4eab
Tom Pearson <tpearson@HeadlandEnt.local>
2018-06-11 13:57:22 +0100

    working on Conditionals and Loops

--- Backend/IntermediateExpress/EJSDemo/app.js
+++ Backend/IntermediateExpress/EJSDemo/app.js
@@ -10,6 +10,16 @@ app.get('/fallinlovewith/:thing', function(req,res){
     res.render('love.ejs', {thingVar: thing});
 })
 
+app.get('/posts', function(req,res){
+    var posts = [
+        {title: 'Post 1', author: 'Susy'},
+        {title: 'Adobrable pet bunny', author: 'Phil'},
+        {title: 'woo', author: 'Dave'}
+    ];
+
+    res.render('posts.ejs', {posts: posts});
+})
+
 app.listen(3000, process.env.IP, function(){
     console.log('server is listening');
 })
