15574dd880864e14ea98538541876e116445658e
Tom Pearson <tpearson@HeadlandEnt.local>
2018-06-25 14:22:11 +0100

    nearly fin movie API

--- Backend/APIs/movie_search_app/app.js
+++ Backend/APIs/movie_search_app/app.js
@@ -3,12 +3,18 @@ var app = express();
 var request = require('request');
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res){
+    res.render('search');
+});
+
 app.get('/results', function(req, res){
-    request('http://www.omdbapi.com/?s=california&apikey=thewdb', function(error, response, body){
+   var query = req.query.search;
+    var url = 'http://www.omdbapi.com/?s=' + query + '&apikey=thewdb';
+    request(url, function(error, response, body){
         if (!error && response.statusCode == 200){
-            var results = JSON.parse(body);
+            var data = JSON.parse(body);
         
-            res.render('results');
+            res.render('results', {data: data});
         }
     });
 });
