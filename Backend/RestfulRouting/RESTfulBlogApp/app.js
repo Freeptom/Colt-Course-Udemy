be7fcd5348b3aa06d618619ad7bf88fd3bfab7c1
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-07-11 07:02:15 +0100

    finished SHOW

--- Backend/RestfulRouting/RESTfulBlogApp/app.js
+++ Backend/RestfulRouting/RESTfulBlogApp/app.js
@@ -41,10 +41,29 @@ app.get('/blogs/new', function(req, res){
 });
 
 // CREATE ROUTE
+app.post('/blogs', function(req, res){
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render('new');
+        }
+        else {
+            res.redirect('/blogs');
+        }
+    });
+    //redirect
+});
 
-
-
-// RESTful Routes
+// SHOW ROUTE
+app.get('/blogs/:id', function(req,res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.render('show', {blog: foundBlog});
+        }
+    })
+});
 
 
 app.listen(3000, function(){
