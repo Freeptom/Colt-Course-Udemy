7a04210aa03f19f50fe7b649b13644a0d8fe40d4
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-07-29 19:29:49 +0100

    finished AuthDemo

--- Backend/Authentication/AuthDemo/app.js
+++ Backend/Authentication/AuthDemo/app.js
@@ -20,6 +20,7 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -30,17 +31,17 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
-app.get('/secret', function(req, res){
+app.get('/secret', isLoggedIn, function(req, res){
     res.render('secret');
 });
 
-// =============
+
 // AUTH ROUTES
-// =============
+
 //show sign up form
 app.get('/register', function(req,res){
     res.render('register');
-})
+});
 // handling user sign up
 app.post('/register', function(req,res){
     req.body.username
@@ -55,6 +56,32 @@ app.post('/register', function(req,res){
         });
     });
 });
+
+// LOGIN ROUTES
+// render login form
+app.get("/login", function(req,res){
+    res.render("login");
+});
+//login logic
+// middleware
+app.post("/login",passport.authenticate('local', {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req,res){
+});
+
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+//if logged in go to secret 'next' else redirect to login page
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    } 
+    res.redirect("/login")
+};
 
 app.listen(3000, function(){
     console.log('Server started');
