83ad07e10902cf6c1204e034b7a608b4c66330c9
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-07-29 17:01:37 +0100

    finished 303

--- Backend/Authentication/AuthDemo/app.js
+++ Backend/Authentication/AuthDemo/app.js
@@ -1,16 +1,59 @@
-var express = require('express');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/auth_demo_app');
+var express                = require('express'), 
+    mongoose               = require('mongoose'),
+    passport               = require('passport'),
+    bodyParser             = require('body-parser'),
+    User                   = require('./models/user'),
+    LocalStrategy          = require('passport-local'),
+    passportLocalMongoose  = require('passport-local-mongoose');
 
+mongoose.connect('mongodb://localhost/auth_demo_app');
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(require('express-session')({
+    secret: "Rust is the best and cutest dog in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// =============
+// ROUTES
+// =============
 app.get('/', function(req, res){
     res.render('home');
 });
 
 app.get('/secret', function(req, res){
     res.render('secret');
+});
+
+// =============
+// AUTH ROUTES
+// =============
+//show sign up form
+app.get('/register', function(req,res){
+    res.render('register');
+})
+// handling user sign up
+app.post('/register', function(req,res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req,res, function(){
+            res.redirect('/secret');
+        });
+    });
 });
 
 app.listen(3000, function(){
