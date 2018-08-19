4b55d0f3cc53ceda01c20ceb3cf660a415f93f37
Freeptom <32514405+Freeptom@users.noreply.github.com>
2018-08-19 16:37:06 +0100

    added deployed back into folder

--- Backend/YelpCamp/v12Deployed/app.js
+++ Backend/YelpCamp/v12Deployed/app.js
@@ -16,7 +16,10 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require('./routes/campgrounds'),
     indexRoutes          = require('./routes/index')
 
-mongoose.connect('mongodb://localhost/yelp_camp');
+
+var url = process.env.DATABASEURL || "mongodb://localhost/yelpcamp";
+mongoose.connect(url);
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
@@ -53,6 +56,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log('The YelpCamp Server Has Started!');
 });
